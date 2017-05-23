d730da46eb35887ad7aaa5be960d5594b3bb7f18
Soheil RamezanZadeh <soheillamso@gmail.com>
2017-05-23 09:32:46 +0430

    d

--- Public.cs
+++ Public.cs
@@ -60,6 +60,8 @@ namespace SRL
     }
     public class Security
     {
+        public Security() { }
+
         public enum UserRegistrationStatus
         {
             NotRegistered = 0,
@@ -119,7 +121,21 @@ namespace SRL
                 return true;
             }
         }
-      
+      public string GetSHA1(string input)
+        {
+            using (System.Security.Cryptography.SHA1Managed sha1=new System.Security.Cryptography.SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+                return sb.ToString();
+
+            }
+        }
+
     }
     public class KeyValue
     {
