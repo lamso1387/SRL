e65edb80211fe9b9c3fa171db6f09238fb52f43f
Soheil RamezanZadeh <soheillamso@gmail.com>
2017-06-14 18:05:54 +0430

    s

--- Public.cs
+++ Public.cs
@@ -515,14 +515,28 @@ namespace SRL
             parent.Controls.Add(child);
             AliagnChildToParent(parent, child);
         }
-        public void AddChildToParentControlsZoomAndAliagn(Control parent, Control child, decimal font_factor = 1)
+        public void AddChildToParentControlsZoomAndAliagn(Control parent, Control child, decimal font_factor = 1, bool use_parent_font_family=false)
         {
+            FontFamily font_family = child.Font.FontFamily;
+            if (use_parent_font_family) font_family = parent.Font.FontFamily;
+
+            
+
+            FontStyle font_style = font_family.IsStyleAvailable(FontStyle.Regular) ? FontStyle.Regular :
+                font_family.IsStyleAvailable(FontStyle.Bold) ? FontStyle.Bold :
+                font_family.IsStyleAvailable(FontStyle.Italic) ? FontStyle.Italic :
+                font_family.IsStyleAvailable(FontStyle.Underline) ? FontStyle.Underline : FontStyle.Strikeout;
+
+            child.Font = new Font(font_family, child.Font.Size * (float)font_factor, font_style);
+
             decimal x_relative = Decimal.Divide(parent.Width, child.Width);
             decimal y_relative = Decimal.Divide(parent.Height, child.Height);
             var f = (x_relative + y_relative) / 2;
             f *= font_factor;
 
-            child.Font = new Font(child.Font.FontFamily, child.Font.Size * (float)f);
+
+
+            child.Font = new Font(font_family, child.Font.Size * (float)f, font_style);
 
             AddChildToParentControlsAliagn(parent, child);
         }
@@ -543,6 +557,16 @@ namespace SRL
 
             child.Width = int.Parse(Math.Floor(child_width_relative * form_x).ToString());
             child.Height = int.Parse(Math.Floor(child_height_relative * form_y).ToString());
+        }
+        public void AdjustAndAliagnChildToParent(Control parent_form, Control child, double child_width_relative, double child_height_relative)
+        {
+            int form_x = parent_form.Width;
+            int form_y = parent_form.Height;
+
+            child.Width = int.Parse(Math.Floor(child_width_relative * form_x).ToString());
+            child.Height = int.Parse(Math.Floor(child_height_relative * form_y).ToString());
+
+            AliagnChildToParent(parent_form, child);
         }
 
         public class Media
