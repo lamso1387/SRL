fadb4d3488c3445ef8427e2e4211c31ae3bfcc0b
soheil <soheillamso@gmail.com>
2017-07-02 00:59:51 +0430

    s

--- Public.cs
+++ Public.cs
@@ -87,6 +87,11 @@ namespace SRL
     }
     public class SettingClass<SettingEntity> where SettingEntity : class
     {
+        class DefaultSetting
+        {
+           public string key {get;set;}
+           public string value { get; set; }
+        }
         SRL.Database srl_database = new Database();
         SRL.ClassManagement<SettingEntity> class_mgnt = new SRL.ClassManagement<SettingEntity>();
         string setting_table_name;
@@ -144,12 +149,13 @@ namespace SRL
 
             try
             {
+            
                 Dictionary<string, string> default_setting = new Dictionary<string, string>();
                 string sql = "select key, value from " + setting_table_name;
-                var query_rows = srl_database.SqlQuery<SettingEntity>(db, sql);
+                var query_rows = srl_database.SqlQuery<DefaultSetting>(db, sql);
                 foreach (var row in query_rows)
                 {
-                    default_setting[class_mgnt.GetProperty("key", row).ToString()] = class_mgnt.GetProperty("value", row).ToString();
+                    default_setting[row.key] = row.value;
                 }
                 default_setting.Remove("default_setting");
                 default_setting.Remove("db_version");
@@ -170,7 +176,7 @@ namespace SRL
                 }
             }
             catch (Exception exc)
-            {
+           {
                 error = exc.Message;
             }
             return error;
@@ -265,7 +271,7 @@ namespace SRL
             int row_count = db.Set<SettingEntity>().Count();
             return query == null || query == "false" ? false : true;
         }
-        private string SqlQuerySettingTable(string key, string default_if_empty = null)
+        public string SqlQuerySettingTable(string key, string default_if_empty = null)
         {
 
             string sql = "select value from " + setting_table_name + " where key='" + key + "'";
@@ -637,6 +643,105 @@ namespace SRL
     }
     public class WinTools
     {
+        /// <summary>
+        /// Add column show/hide capability to a DataGridView. When user right-clicks 
+        /// the cell origin a popup, containing a list of checkbox and column names, is
+        /// shown. 
+        /// example:  DataGridViewColumnSelector cs = new DataGridViewColumnSelector(dataGridView1);
+        /// cs.PopupMaxHeight = 100;
+        /// cs.PopupWidth = 110;
+        /// </summary>
+        public class DataGridViewColumnSelector
+        {
+            // the DataGridView to which the DataGridViewColumnSelector is attached
+            private DataGridView mDataGridView = null;
+            // a CheckedListBox containing the column header text and checkboxes
+            private CheckedListBox mCheckedListBox;
+            // a ToolStripDropDown object used to show the popup
+            private ToolStripDropDown mPopup;
+
+            /// <summary>
+            /// The max height of the popup
+            /// </summary>
+            public int PopupMaxHeight = 300;
+            /// <summary>
+            /// The width of the popup
+            /// </summary>
+            public int PopupWidth = 200;
+
+            /// <summary>
+            /// Gets or sets the DataGridView to which the DataGridViewColumnSelector is attached
+            /// </summary>
+            public DataGridView DataGridView
+            {
+                get { return mDataGridView; }
+                set
+                {
+                    // If any, remove handler from current DataGridView 
+                    if (mDataGridView != null) mDataGridView.CellMouseClick -= new DataGridViewCellMouseEventHandler(mDataGridView_CellMouseClick);
+                    // Set the new DataGridView
+                    mDataGridView = value;
+                    // Attach CellMouseClick handler to DataGridView
+                    if (mDataGridView != null) mDataGridView.CellMouseClick += new DataGridViewCellMouseEventHandler(mDataGridView_CellMouseClick);
+                }
+            }
+
+            // When user right-clicks the cell origin, it clears and fill the CheckedListBox with
+            // columns header text. Then it shows the popup. 
+            // In this way the CheckedListBox items are always refreshed to reflect changes occurred in 
+            // DataGridView columns (column additions or name changes and so on).
+            void mDataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+            {
+                if (e.Button == MouseButtons.Right && e.RowIndex == -1 && e.ColumnIndex == -1)
+                {
+                    mCheckedListBox.Items.Clear();
+                    foreach (DataGridViewColumn c in mDataGridView.Columns)
+                    {
+                        mCheckedListBox.Items.Add(c.HeaderText, c.Visible);
+                    }
+                    int PreferredHeight = (mCheckedListBox.Items.Count * 16) + 7;
+                    mCheckedListBox.Height = (PreferredHeight < PopupMaxHeight) ? PreferredHeight : PopupMaxHeight;
+                    mCheckedListBox.Width = this.PopupWidth;
+                    int x_location = e.X;
+                    if (mDataGridView.RightToLeft == RightToLeft.Yes) x_location +=  mDataGridView.Width;
+
+                    mPopup.Show(mDataGridView.PointToScreen(new Point(x_location, e.Y)));
+                }
+            }
+
+            // The constructor creates an instance of CheckedListBox and ToolStripDropDown.
+            // the CheckedListBox is hosted by ToolStripControlHost, which in turn is
+            // added to ToolStripDropDown.
+            public DataGridViewColumnSelector()
+            {
+                mCheckedListBox = new CheckedListBox();
+                mCheckedListBox.CheckOnClick = true;
+                mCheckedListBox.ItemCheck += new ItemCheckEventHandler(mCheckedListBox_ItemCheck);
+
+                ToolStripControlHost mControlHost = new ToolStripControlHost(mCheckedListBox);
+                mControlHost.Padding = Padding.Empty;
+                mControlHost.Margin = Padding.Empty;
+                mControlHost.AutoSize = false;
+
+                mPopup = new ToolStripDropDown();
+                mPopup.Padding = Padding.Empty;
+                mPopup.Items.Add(mControlHost);
+            }
+
+            public DataGridViewColumnSelector(DataGridView dgv)
+                : this()
+            {
+                this.DataGridView = dgv;
+            }
+
+            // When user checks / unchecks a checkbox, the related column visibility is 
+            // switched.
+            void mCheckedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+            {
+                mDataGridView.Columns[e.Index].Visible = (e.NewValue == CheckState.Checked);
+            }
+        }
+
         public class ComboItem
         {
             public string Text { get; set; }
