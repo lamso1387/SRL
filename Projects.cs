8bcd80cb8392fcb038fdf42d6148aacaf783fba6
Soheil Ramezanzadeh <soheillamso@gmail.com>
2018-01-27 15:55:05 +0330

    l

--- Projects.cs
+++ Projects.cs
@@ -40,7 +40,7 @@ namespace SRL
 
                         int count = list.Count();
                         foreach (var item in list)
-                        { 
+                        {
 
                             try
                             {
@@ -111,9 +111,7 @@ namespace SRL
                     public string State { get; set; }
                     public string TownShip { get; set; }
                     public string Village { get; set; }
-
                 }
-
                 public static void Estelam(string file_full_path, string table_name, string api_key)
                 {
                     DataTable table = SRL.AccessManagement.GetDataTableFromAccess(file_full_path, table_name);
@@ -170,13 +168,26 @@ namespace SRL
 
                         try
                         {
+                            string address = "";
+
+                            if (result.ErrorCode ==0)
+
+                            {
+                                address += "استان " + result.State.Trim() + "- شهرستان " + result.TownShip.Trim() + "- بخش " +result.Zone.Trim()+ "- " + result.LocationType.Trim() + " " + result.Location.Trim()
+                                + "- " + result.Parish.Trim() + "- " + result.PreAvenue.Trim() + "- " + result.Avenue.Trim();
+                                if (result.HouseNo != 0) address += "- پلاک " + result.HouseNo;
+                                if (!string.IsNullOrWhiteSpace(result.BuildingName)) address += "- ساختمان " + result.BuildingName.Trim();
+                                address += "- طبقه " + result.FloorNo.Trim();
+                                if (!string.IsNullOrWhiteSpace(result.SideFloor)) address += "- واحد " + result.SideFloor.Trim();
+                            }
+
                             string query = "update " + args[4] + " set status='OK', correct='" + (result.ErrorCode == 0 ?
                        "true" : "false") + "', ErrorCode=" + result.ErrorCode + " , ErrorMessage='" + result.ErrorMessage + "', "
                        + " Location='" + result.Location + "' , LocationCode=" + result.LocationCode + " "
                                 + " , LocationType='" + result.LocationType + "'  , State='" + result.State + "'  , TownShip='" + result.TownShip + "'"
-                                + "  , Village='" + result.Village + "'   where PostCode='" + item.PostCode + "' ;";
+                                + "  , Village='" + result.Village + "' , Address='" + address + "'  where PostCode='" + item.PostCode + "' ";
 
-                            SRL.AccessManagement.ExecuteToAccess(query, args[3].ToString(), false);
+                            var exce = SRL.AccessManagement.ExecuteToAccess(query, args[3].ToString(), false);
 
 
                         }
@@ -187,11 +198,11 @@ namespace SRL
                         }
                     }
                 }
-                 
+
                 public static void EstelamFromPost(string file_full_path, string table_name, string api_key, string parallel, string password, string username)
                 {
                     DataTable table = SRL.AccessManagement.GetDataTableFromAccess(file_full_path, table_name);
-                    List<GetAddressByPostServerResult> list = SRL.Convertor.ConvertDataTableToList<GetAddressByPostServerResult>(table).Where(x=>x.status !="OK" || x.status==null || x.status=="").ToList();
+                    List<GetAddressByPostServerResult> list = SRL.Convertor.ConvertDataTableToList<GetAddressByPostServerResult>(table).Where(x => x.status != "OK" || x.status == null || x.status == "").ToList();
                     PostCodeServiceReference.PostCodeClient client = new PostCodeServiceReference.PostCodeClient();
                     SRL.ActionManagement.MethodCall.ParallelMethodCaller.ParallelCall<GetAddressByPostServerResult>(list, parallel, ParallelAddressByPostServer, null, null, password, client, username, file_full_path, table_name);
                 }
@@ -359,7 +370,8 @@ namespace SRL
                    (PostCodeServiceReference.PostCodeClient client, string username, string post_code, string password)
             {
                 string hash = ComputePostCodeHash(password, post_code);
-                return client.GetAddressByPostcode(username, hash, post_code, "", "", "");
+                var res = client.GetAddressByPostcode(username, hash, post_code, "", "", "");
+                return res;
             }
             public static PostalCodeFromPostClass EstelamPostalCodeFromPost(string postal_code, out HttpResponseMessage response)
             {
